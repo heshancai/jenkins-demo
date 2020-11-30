0dc932d25ada5d76c446c00d3740a3d82968d2f4
heshancai <2840137176@qq.com>
2020-11-30 15:53:02 +0800

    初始化

---
+++ src/main/java/com/example/jenkinsdemo/controller/JenkinsTestController.java
@@ -0,0 +1,21 @@
+package com.example.jenkinsdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 描述：
+ *
+ * @author heshancai
+ * @Package com.example.jenkinsdemo.controller
+ * @data 2020/11/30 15:44
+ */
+@RestController
+public class JenkinsTestController {
+
+    @RequestMapping("/testJenkins")
+    public String testJenkins(){
+        return "测试jenkins构建发布成功java程序";
+    }
+
+}
